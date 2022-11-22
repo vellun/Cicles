99a5c38dac78ce18374ed36df6c6e62ac06349cc
vellun <moonpassiflora@gmail.com>
2022-11-22 16:27:13 +0300

    Теперь круги разных цветов

--- Cicles/main2.py
+++ Cicles/main2.py
@@ -4,13 +4,13 @@ from PyQt5.QtGui import QPainter, QColor, QPainterPath
 from PyQt5.QtWidgets import QMainWindow, QApplication
 import random
 from PyQt5.QtCore import Qt
-from PyQt5 import uic
+import UI
 
 
-class Window(QMainWindow):
+class Window(QMainWindow, UI.Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.flag = False
         self.pushButton.clicked.connect(self.result)
 
@@ -22,18 +22,19 @@ class Window(QMainWindow):
             self.qp.end()
 
     def draw(self):
+        self.colors = [random.randrange(255), random.randrange(255), random.randrange(255)]
         self.coords = [random.randrange(596), random.randrange(727)]
         size = random.randrange(300)
-        self.qp.setBrush(QColor(255, 255, 0))
+        self.qp.setBrush(QColor(*self.colors))
         self.qp.drawEllipse(*self.coords, size, size)
 
     def result(self):
         self.flag = True
         self.update()
 
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Window()
     ex.show()
     sys.exit(app.exec())
-
